71e151a9af9e107208282e2f2a5f49d0e1337855
zhangaifei <1334806067@qq.com>
2021-08-07 21:28:40 +0800

    WIP: add scene

--- examples/scene/scene01/src/App.js
+++ examples/scene/scene01/src/App.js
@@ -3,16 +3,16 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div class="page-bg"></div>
+      <div className="page-bg"></div>
 
-      <div class="animation-wrapper">
-        <div class="particle particle-1"></div>
-        <div class="particle particle-2"></div>
-        <div class="particle particle-3"></div>
-        <div class="particle particle-4"></div>
+      <div className="animation-wrapper">
+        <div className="particle particle-1"></div>
+        <div className="particle particle-2"></div>
+        <div className="particle particle-3"></div>
+        <div className="particle particle-4"></div>
       </div>
 
-      <div class="page-wrapper"> 
+      <div className="page-wrapper"> 
         <h1>Iphone</h1>
       </div>
 
