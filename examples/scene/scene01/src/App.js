a09e1575edf534539b10009ff73375a238ee55e3
zhangaifei <1334806067@qq.com>
2021-08-07 20:52:20 +0800

    feat: add scene

---
+++ examples/scene/scene01/src/App.js
@@ -0,0 +1,23 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="page-bg"></div>
+
+      <div class="animation-wrapper">
+        <div class="particle particle-1"></div>
+        <div class="particle particle-2"></div>
+        <div class="particle particle-3"></div>
+        <div class="particle particle-4"></div>
+      </div>
+
+      <div class="page-wrapper"> 
+        <h1>Iphone</h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
